98a85365ac42594ae0d68ae38661724f10e4d020
agrawalshubham1012 <50662055+agrawalshubham1012@users.noreply.github.com>
2022-01-06 21:26:25 +0530

    Other page added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import homeSlice from '../pages/Home/homeSlice';
+import FrontPageSlice from '../pages/FrontPage/FrontPageSlice';
 
 export const store = configureStore({
   reducer: {
     home: homeSlice,
+    frontPage: FrontPageSlice,
   },
 });
